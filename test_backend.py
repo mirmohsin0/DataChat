3a8fc7febe42136ed33123899f1a00b5664c2d35
Moh Sin <foyerturf990@gmail.com>
2026-05-02 02:28:37 +0530

    Update test_backend.py

--- test_backend.py
+++ test_backend.py
@@ -1,6 +1,6 @@
 from backend import *
 
-print(add_message("sin", "hello world"))
+print(add_message("sin", "i am sin"))
 print(get_all_messages())
 print(search_user("sin"))
 print(search_keyword("hello"))
